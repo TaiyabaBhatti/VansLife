16eb6fb25b999556d9ed23f64fe8c8349caa1be9
TaiyabaBhatti <taiyababhattii@gmail.com>
2025-06-26 11:51:39 +0500

    implemented Outlet component and i wrapped the entire routes inside <Layout> Component

--- src/routing/Layout.jsx
+++ src/routing/Layout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
+Outlet;
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      {children}
+      {/* {children} */}
+      <Outlet />
       <Footer />
     </>
   );
